bf894299d5019eff89783e96d400da45bd916985
NazarenkoAnna2021.com <93328568+NazarenkoAnna2021@users.noreply.github.com>
2022-01-04 22:01:36 +0200

    position

---
+++ src/ts/index.ts
@@ -0,0 +1,13 @@
+import '/css/style.css'
+import { dom } from './dom'
+import View from './view'
+import Controller from './controller';
+
+
+const view = new View();
+const controller = new Controller();
+view.writeLi();
+
+dom.addButton.addEventListener('click', controller.addElem);
+dom.toDo.addEventListener('click', controller.deleteElement);
+dom.toDo.addEventListener('change', controller.checkTask);
